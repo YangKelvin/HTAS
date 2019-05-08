f7de8fa2b0c5304e963d84b631ce397c2654faec
KelvinYang <t105590045@ntut.org.tw>
2019-05-09 01:33:20 +0800

    ptt:crawler test_parse_failed

--- HTAS/PttWebCrawler/crawler.py
+++ HTAS/PttWebCrawler/crawler.py
@@ -1,2 +1,18 @@
+import requests
+import json
+
+
 class PttWebCrawler():
-    print('a')
+    PTT_URL = 'https://www.ptt.cc'
+
+    def __init__():
+        pass
+
+    @staticmethod
+    def parse(link, timeout=3):
+        # resp = requests.get(url=link, cookies={'over18': '1'}, verify=VERIFY, timeout=timeout)
+        resp = requests.get(url=link, cookies={'over18': '1'}, timeout=timeout)
+        # print('status code: %d' % (resp.status_code))
+        if (resp.status_code != 200):
+            print('invalid url:', resp.url)
+            return json.dumps({"error": "invalid url"}, sort_keys=True, ensure_ascii=False)
--- HTAS/test/ut_ptt_crawler.py
+++ HTAS/test/ut_ptt_crawler.py
@@ -5,11 +5,12 @@ from ..PttWebCrawler.crawler import PttWebCrawler as crawler
 
 
 class TestCrawler(unittest.TestCase):
-    def test_parse(self):
-        self.link = 'https://www.ptt.cc/bbs/Gossiping/M.1557331202.A.58F.html'
-        self.article_id = 'M.1557331202.A.58F'
-        self.board = 'Gossiping'
+    def test_parse_failed(self):
+        self.link = 'https://www.ptt.cc/bbs/Gossiping/failure.html'
+        crawler_data = crawler.parse(self.link)
+        json_data = json.loads(crawler_data)  # str to dict
+        self.assertEqual('invalid url', json_data['error'])
 
-        json_data = json.loads(crawler.parse(self.link))
-        self.assertEqual(json_data['article_id'], self.article_id)
-        self.assertEqual(json_data['borad'], self.borad)
+
+if __name__ == '__main__':
+    unittest.main()
