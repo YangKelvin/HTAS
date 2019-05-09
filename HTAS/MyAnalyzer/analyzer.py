e04149bb37808b2716a8fc36517dc7c00709f1e0
KelvinYang <t105590045@ntut.org.tw>
2019-05-09 14:03:54 +0800

    close file

--- HTAS/MyAnalyzer/analyzer.py
+++ HTAS/MyAnalyzer/analyzer.py
@@ -6,4 +6,5 @@ class Analyzer():
     def read_ptt_json(url_json):
         read_file = open(url_json, 'r')
         data = json.load(read_file) # dict
+        read_file.close()
         return data
