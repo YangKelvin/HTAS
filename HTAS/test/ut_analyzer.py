422393ac7c6804d34b01da00407509806cc127e9
KelvinYang <t105590045@ntut.org.tw>
2019-05-09 13:53:17 +0800

    test_read_ptt_json2

--- HTAS/test/ut_analyzer.py
+++ HTAS/test/ut_analyzer.py
@@ -15,6 +15,14 @@ class TestAnalyzer(unittest.TestCase):
         self.assertEqual('M.1557332400.A.08C', self.text['article_id'])
         self.assertEqual('Re: [爆卦] 台灣一定會被統一，因為經濟', self.text['article_title'])
 
+    def test_read_ptt_json2(self):
+        self.text = self.analyzer.read_ptt_json(
+            'HTAS/test/test_tmp/Gossiping-39001-39100.json')
+        self.assertEqual('M.1557139088.A.5B2',
+                         self.text['articles'][0]['article_id'])
+        self.assertEqual('[問卦] 超人何時再起', self.text['articles']
+                         [0]['article_title'])
+
 
 if __name__ == '__main__':
     unittest.main()
