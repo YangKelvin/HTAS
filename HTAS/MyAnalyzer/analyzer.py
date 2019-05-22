afc0a20867d3678dd9ecc491bc189971cdfe0cfc
KelvinYang <t105590045@ntut.org.tw>
2019-05-22 23:22:32 +0800

    fix stop word

--- HTAS/MyAnalyzer/analyzer.py
+++ HTAS/MyAnalyzer/analyzer.py
@@ -1,6 +1,6 @@
 import jieba
 import json
-
+import pandas
 
 class Analyzer():
 
@@ -16,7 +16,7 @@ class Analyzer():
         jieba.add_word('fb')
 
         stopWords = []
-        with open('./stops.txt', 'r', encoding='utf-8') as stop_file:
+        with open('HTAS/MyAnalyzer/stops.txt', 'r', encoding='utf-8') as stop_file:
             for stop in stop_file.readlines():
                 stop = stop.strip()
                 stopWords.append(stop)
@@ -32,3 +32,23 @@ class Analyzer():
     def cut(text, is_cut_all=False, is_HMM=True):
         seg_list = jieba.cut(text, cut_all=is_cut_all, HMM=is_HMM)
         return seg_list
+
+    @staticmethod
+    def analysis_articles():
+        titles = []
+        contents = []
+        totalLen = []
+        df = pd.DataFrame(columns=['Words', 'Vec', 'TF', 'IDF'])
+        for i in range(len(data['articles'])):
+            # title分詞並去除停用詞
+            title = jieba.cut(data['articles'][i]['article_title'], cut_all=False)
+            title = list(filter(lambda t: t not in stopWords and t != ' ' and t != '\u3000', title))
+            # 去除文章中的URL
+            data['articles'][i]['content'] = re.sub(r"http\S+", '', data['articles'][i]['content'], flags=re.MULTILINE)
+            # content分詞並去除停用詞
+            content = jieba.cut(data['articles'][i]['content'], cut_all=False)
+            content = list(filter(lambda c : c not in stopWords and c != ' ' and c != '\u3000', content))
+            
+            totalLen.append(len(title) + len(content))
+            titles.append(title)
+            contents.append(content)
