47127198824c56a0c6933cae9fe9496a6a839e3c
kelvin_yang <kelvin_yang@gss.com>
2019-08-25 14:54:44 +0800

    開始 my_crawer

---
+++ HTAS/my_crawler.py
@@ -0,0 +1,7 @@
+from PttWebCrawler.crawler import PttWebCrawler as ptt_crawler
+
+my_ptt_crawler = ptt_crawler()
+# my_ptt_crawler.parse_articles(39279, 39279, 'Gossiping')
+
+my_ptt_crawler.parse_articles(38773, 38774, 'Gossiping')
+# my_ptt_crawler.parse_articles(39101, 39200, 'Gossiping')
