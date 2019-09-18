8cc3a94d22b73d18d83279ac893e8117a42d1d58
kelvin_yang <kelvin_yang@gss.com>
2019-09-18 13:20:37 +0800

    WEB 完成網頁外觀

--- HTAS_WEB/visual.py
+++ HTAS_WEB/visual.py
@@ -16,3 +16,25 @@ bp = Blueprint('visual', __name__, url_prefix='/visual')
 def test_bar():
     data = {'Chrome': 52.9, 'Opera': 1.6, 'Firefox': 27.7}
     return render_template('visual/test_bar.html', data=data)
+
+# 分析畫面
+@bp.route('/analysis')
+def analysis():
+    fake_data = get_fate_data()
+
+    return render_template('visual/analysis.html', posts=fake_data)
+
+
+def get_fate_data():
+    fake_data = []
+    
+    # 標題、正面、中立、負面
+    data1 = {'title': 'title1', 'data':{ 'positive': 60.0, 'neutral': 10.0, 'negative': 30.0}}
+    data2 = {'title': 'title2', 'data':{ 'positive': 65.0, 'neutral': 15.0, 'negative': 20.0}}
+    data3 = {'title': 'title3', 'data':{ 'positive': 65.0, 'neutral': 20.0, 'negative': 15.0}}
+
+    fake_data.append(data1)
+    fake_data.append(data2)
+    fake_data.append(data3)
+
+    return fake_data
